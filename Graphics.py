55117295a2588f29a34cc6bb5456edcacbaffcd5
gyorfia <gyorfia9@gmail.com>
2024-09-21 23:33:50 +0200

    Ising model with 2D quiver plotting

---
+++ Graphics.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Graphics:
+    def __init__(self, N):
+        """
+        N by N grid
+        """
+        # Create grid coordinates
+        self.X, self.Y = np.meshgrid(np.arange(N), np.arange(N))
+        # Define the arrow directions: (0, +1) for spin up, (0, -1) for spin down
+        self.U = np.zeros((N, N))  # X-component of arrow is 0 (no horizontal movement)
+        self.fig, self.ax = plt.subplots(figsize=(6,6))
+        # quiver('grid x coordinates', 'grid y coordinates', 'vector x coordinates', 'vector y coordinates', ...)
+        self.quiver = plt.quiver(self.X, self.Y, self.U, self.X, pivot='middle', scale=20, color='black')
+        self.ax.set_title('Ising Model Spin Configuration')
+        self.ax.invert_yaxis()  # Invert y-axis to match typical matrix visualization
+        self.ax.set_aspect('equal')  # Ensure square grid
+        self.ax.set_xticks([]) # Remove x-ticks for cleaner look
+        self.ax.set_yticks([])  # Remove y-ticks for cleaner look
+        plt.ion  # Turn on interactive mode
+
+    def UpdateModel(self, spinMat):
+        self.quiver.set_UVC(self.U, spinMat) # Update the quiver with new U and V components
+        plt.draw()
+        plt.pause(0.1)
+
+    def FinalPlot(self):
+        print("Final plot\n")
+        plt.ioff()
+        plt.show()
--- spin.py
+++ spin.py
@@ -1,1 +1,23 @@
-print("Hello world!")
+import Graphics as gfx
+import numpy.random as random
+
+# INIT
+N = 10
+J = 1
+gfx = gfx.Graphics(N)
+random.seed(1)
+spinMat = random.choice([-1, 1], size=(N, N))
+flips = 0
+
+# LOGIC
+for k in range(N): # how many loops on the whole grid
+    for i in range(N):
+        for j in range(N):
+            E1 = J*spinMat[i][j]*(spinMat[i-1][j] + spinMat[(i+1)%N][j] + spinMat[i][j-1] + spinMat[i][(j+1)%N]) # negative indexing is implemented
+            E2 = J*(-1)*spinMat[i][j]*(spinMat[i-1][j] + spinMat[(i+1)%N][j] + spinMat[i][j-1] + spinMat[i][(j+1)%N]) # energy if flipped
+            if(E2 < E1):
+                spinMat[i][j] *= -1
+                flips +=1
+    gfx.UpdateModel(spinMat)
+    print(f"Updating, flip count: {flips}\n")
+gfx.FinalPlot()
