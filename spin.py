7bfec2436b56e6bd5dbc098b0ce71e5e9f452113
bnyari <nyaribendeguz94@gmail.com>
2024-09-25 21:02:45 +0200

    See the comments for the details. The plot is a very beautyful work!

--- spin.py
+++ spin.py
@@ -10,7 +10,11 @@ spinMat = random.choice([-1, 1], size=(N, N))
 flips = 0
 
 # LOGIC
-for k in range(N): # how many loops on the whole grid
+# Bendeguz: use much more iterations!
+# there are N*N sites --> you need to loop over all sites many times, or at least a few times
+for k in range(N**3): # how many loops on the whole grid
+    #Bendeguz: instead of going through the whole system pick a random site!!
+    #change the upcoming two for loops to randomly picking i and j
     for i in range(N):
         for j in range(N):
             E1 = J*spinMat[i][j]*(spinMat[i-1][j] + spinMat[(i+1)%N][j] + spinMat[i][j-1] + spinMat[i][(j+1)%N]) # negative indexing is implemented
