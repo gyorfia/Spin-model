fa3e0b3587affb81d3dcd0b9e5e037a0faedda81
gyorfia <gyorfia9@gmail.com>
2024-10-09 13:32:16 +0200

    copied current state from Matyi based on the newest state of main

--- spin.py
+++ spin.py
@@ -4,7 +4,6 @@ import math
 
 #INIT
 N = 500
-TPrecision = 100
 J = -1
 iteration=0
 k=math.pow(N,2.179)
@@ -14,11 +13,8 @@ flips = 0
 seed = 1234
 np.random.seed(seed)
 rng1=np.random.default_rng(seed)
-#tx = np.linspace(0.1, 5, TPrecision)
-#ty = np.zeros(TPrecision)
 tc=2/(np.log(1+np.sqrt(2)))
 #LOGIC
-#for l in range(len(tx)):
 while(iteration <k): # how many loops on the whole grid
         i=rng1.integers(0,N)
         j=rng1.integers(0,N)
@@ -30,10 +26,4 @@ while(iteration <k): # how many loops on the whole grid
             flips +=1
         iteration +=1
         print(iteration)
-    #for i in range(N):
-        #for j in range(N):
-            #ty[l] += J*spinMat[i][j]
-    #print(f"Progress: {l*100/TPrecision:.0f}%\n")
-#ty = np.abs(ty) / (N*N)
-        if iteration % (k // 10) == 0:        
-            gfx.Black_White(spinMat, iteration)
+gfx.Black_White(spinMat, iteration)
