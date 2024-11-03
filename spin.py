5ef125d5302049796dd9b0fbc757dbfc2bdb88ef
Ádám Győrfi <gyorfia9@gmail.com>
2024-11-03 18:27:19 +0100

    tdqm progress bar

--- spin.py
+++ spin.py
@@ -1,13 +1,13 @@
 import Graphics as gfx
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from tqdm import tqdm
 
 def BoxIsFilled(spinMat, i0, i1, j0, j1):
     '''
-    (spinMat, starting row, ending row (incl.), starting column, ending column (incl.),)
+    (spinMat, starting row, ending row (incl.), starting column, ending column (incl.))
     '''
     submatrix = spinMat[i0:i1+1, j0:j1+1]
-
     if np.all(submatrix == 1) or np.all(submatrix == -1):
         return False
     return True
@@ -15,52 +15,49 @@ def BoxIsFilled(spinMat, i0, i1, j0, j1):
 # INIT
 N = int(2**8)
 J = -1
-iteration=0
-k=np.pow(N,2.8) # k=np.pow(N,2.179)
+iteration = 0
+k = np.pow(N, 2.8)  # originally: k=np.pow(N,2.179)
 # Change figure dimension in constructor: Graphics(nRows, nColumns)
 gfx = gfx.Graphics(1, 2)
 spinMat = np.random.choice([-1, 1], size=(N, N))
 flips = 0
 seed = 1234
 np.random.seed(seed)
-rng1=np.random.default_rng(seed)
-tc=2/(np.log(1+np.sqrt(2)))
+rng1 = np.random.default_rng(seed)
+tc = 2 / (np.log(1 + np.sqrt(2)))
 
 # SPINMAT CALCULATIONS
-while(iteration <k): # how many loops on the whole grid
-        i=rng1.integers(0,N)
-        j=rng1.integers(0,N)
-        E1 = J*spinMat[i][j]*(spinMat[i-1][j] + spinMat[(i+1)%N][j] + spinMat[i][j-1] + spinMat[i][(j+1)%N])
-        E2 = J*(-1)*spinMat[i][j]*(spinMat[i-1][j] + spinMat[(i+1)%N][j] + spinMat[i][j-1] + spinMat[i][(j+1)%N]) # energy if flipped
-        factor=np.exp((-(E2-E1))/tc)
-        if(E2-E1 <0 or factor> rng1.random()):
-            spinMat[i][j] *= -1
-            flips +=1
-        iteration +=1
-        print(f"Generating spinMat: {(100*iteration/k):.0f}%")
+for _ in tqdm(range(int(k)), desc="Generating spinMat"):
+    i = rng1.integers(0, N)
+    j = rng1.integers(0, N)
+    E1 = J * spinMat[i][j] * (spinMat[i - 1][j] + spinMat[(i + 1) % N][j] + spinMat[i][j - 1] + spinMat[i][(j + 1) % N])
+    E2 = J * (-1) * spinMat[i][j] * (spinMat[i - 1][j] + spinMat[(i + 1) % N][j] + spinMat[i][j - 1] + spinMat[i][(j + 1) % N])  # energy if flipped
+    factor = np.exp((-(E2 - E1)) / tc)
+    if E2 - E1 < 0 or factor > rng1.random():
+        spinMat[i][j] *= -1
+        flips += 1
 
 # DIMENSION COUNTING
-eps = N # first length of the box
-n = N//eps # first size of the box-grid
+eps = 64 # first length of the box
+n = N // eps # first size of the box-grid
 xs_list = []
 ys_list = []
 while eps >= 2: # last length of the box
     nBox = 0
     for i in range(n):
         for j in range(n):
-            if(BoxIsFilled(spinMat, eps*i, eps*(i+1)-1 if i != n-1 else N-1, eps*j, eps*(j+1)-1 if j != n-1 else N-1)):
+            if BoxIsFilled(spinMat, eps * i, eps * (i + 1) - 1 if i != n - 1 else N - 1, eps * j, eps * (j + 1) - 1 if j != n - 1 else N - 1):
                 nBox += 1
     ys_list.append(np.log(nBox))
     xs_list.append(np.log(eps))
     print(f"Current box size: {eps}")
-    eps = int(eps/2)
-    n = N//eps if eps!=0 else 0
+    eps //= 2
+    n = N // eps
 
 # PLOTTING
 xs = np.array(xs_list)
 ys = np.array(ys_list)
 gfx.Black_White(spinMat, N)
-dimension = (ys[len(xs)-1]-ys[0])/(xs[len(xs)-1]-xs[0])
 model = LinearRegression().fit(xs.reshape((-1, 1)), ys)
 gfx.Plot(xs, model.predict(xs.reshape((-1, 1))), f"Dimension: {abs(model.coef_)}", "log(epsilon)", "log(N(epsilon))", False)
 gfx.Scatter_Current(xs, ys)
